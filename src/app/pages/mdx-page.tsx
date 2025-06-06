92b4e9b8526c434df41491c686977fcfd4d3560b
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-06-06 15:58:30 -0500

    experimenting

--- src/app/pages/mdx-page.tsx
+++ src/app/pages/mdx-page.tsx
@@ -1,4 +1,5 @@
 import Welcome from '../markdown/welcome.mdx'
+import Welcome2 from "../markdown/welcome2.mdx"
  
 function CustomH1({ children }: { children: React.ReactNode }) {
   return <h1 style={{ color: 'blue', fontSize: '100px' }}>{children}</h1>
@@ -9,5 +10,5 @@ const overrideComponents = {
 }
  
 export default function Page() {
-  return <Welcome components={overrideComponents} />
+  return <><Welcome2/><Welcome components={overrideComponents} /></>
 }
